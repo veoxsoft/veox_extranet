570bb965b5556b5cc3c190515adecb44cf04a36a
William Condori Quispe <williamccondori@outlook.com>
2019-04-16 08:57:45 -0500

    Reformulacion

--- extranet/urls.py
+++ extranet/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('nosotros', views.nosotros, name='nosotros'),
+    path('mision_vision', views.mision_vision, name='mision_vision'),
+    path('noticias', views.noticias, name='noticias'),
     path('contacto', views.contacto, name='contacto'),
 ]
--- extranet/views.py
+++ extranet/views.py
@@ -8,6 +8,12 @@ def index(request):
 def nosotros(request):
     return render(request, 'nosotros.html')
 
+def mision_vision(request):
+    return render(request, 'mision_vision.html')
+
+def noticias(request):
+    return render(request, 'noticias.html')
+
 def contacto(request):
     return render(request, 'contacto.html')
 
